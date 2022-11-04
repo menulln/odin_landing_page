fef5a9e4ecd0dd408f49653ad793fd6aa32da597
menulln <39300523+menulln@users.noreply.github.com>
2022-11-04 19:23:48 +0100

    Add functionality for validateUsername

--- pages/js/formValidator.js
+++ pages/js/formValidator.js
@@ -7,7 +7,19 @@ inputEmail.addEventListener('input', validateEmail);
 inputPassword.addEventListener('input', validatePassword);
 
 function validateUsername(e) {
+    const re = /[^a-z0-9]/i;
+    
+    const errorMsg = document.createElement('p');
+    errorMsg.textContent = 'Please only enter letters & numbers.';
+    errorMsg.style.color = '#E22F2B';
+    errorMsg.style.fontSize = '14px';
 
+    if (this.value.match(re)) {
+        const section = document.querySelector('.signup-username');
+        if(section.childElementCount < 3) {
+            section.appendChild(errorMsg);
+        }
+    }
 }
 
 function validateEmail(e) {
