19f52cc614fe4dc9c273b252ddb0c1c035e6bedc
menulln <39300523+menulln@users.noreply.github.com>
2022-11-04 19:54:06 +0100

    Add functionality for validatePassword

--- pages/js/formValidator.js
+++ pages/js/formValidator.js
@@ -45,5 +45,27 @@ function validateEmail(e) {
 }
 
 function validatePassword(e) {
+    const section = document.querySelector('.signup-password');
+    const re = /[^a-z0-9!@#$%^&*]/i;
+    const errorMsg = document.createElement('p');
+    errorMsg.textContent = 'At least 8 characters.';
+    errorMsg.style.color = '#E22F2B';
+    errorMsg.style.fontSize = '14px';
 
+    console.log(this.value.length);
+
+    if (this.value.length < 8) {
+        if (section.childElementCount < 3) {
+            section.appendChild(errorMsg);
+        }
+    } else if (this.value.match(re)) {
+        errorMsg.textContent = 'Invalid character. (only A-Z 0-9 !@#$%^&*)';
+        if (section.childElementCount < 3) {
+            section.appendChild(errorMsg);
+        }
+    } else {
+        if (section.childElementCount > 2) {
+            section.removeChild(section.lastChild);
+        }
+    }
 }
