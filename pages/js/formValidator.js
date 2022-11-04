127a6a12e74955137a68b49fa92c72c3c6425d8c
menulln <39300523+menulln@users.noreply.github.com>
2022-11-04 18:12:18 +0100

    Get input elements from ../signup.html

---
+++ pages/js/formValidator.js
@@ -0,0 +1,3 @@
+const inputUsername = document.querySelector('.input-username');
+const inputEmail = document.querySelector('.input-email');
+const inputPassword = document.querySelector('.input-password');
