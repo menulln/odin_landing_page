42a2668c6baba6beb3c6802dca26d3b4e1905ebe
menulln <39300523+menulln@users.noreply.github.com>
2022-11-04 19:25:33 +0100

    Fix validateUsername to remove errorMsg if input is valid

--- pages/js/formValidator.js
+++ pages/js/formValidator.js
@@ -7,17 +7,20 @@ inputEmail.addEventListener('input', validateEmail);
 inputPassword.addEventListener('input', validatePassword);
 
 function validateUsername(e) {
+    const section = document.querySelector('.signup-username');
     const re = /[^a-z0-9]/i;
-    
     const errorMsg = document.createElement('p');
     errorMsg.textContent = 'Please only enter letters & numbers.';
     errorMsg.style.color = '#E22F2B';
     errorMsg.style.fontSize = '14px';
 
     if (this.value.match(re)) {
-        const section = document.querySelector('.signup-username');
-        if(section.childElementCount < 3) {
+        if (section.childElementCount < 3) {
             section.appendChild(errorMsg);
+        }
+    } else {
+        if (section.childElementCount > 2) {
+            section.removeChild(section.lastChild);
         }
     }
 }
