cbf4ea075789188b10d9607ac8b4400fde1201d0
menulln <39300523+menulln@users.noreply.github.com>
2022-11-04 18:15:18 +0100

    Add EventListeners for input fields on change

--- pages/js/formValidator.js
+++ pages/js/formValidator.js
@@ -1,3 +1,7 @@
 const inputUsername = document.querySelector('.input-username');
 const inputEmail = document.querySelector('.input-email');
 const inputPassword = document.querySelector('.input-password');
+
+inputUsername.addEventListener('onchange', validateUsername);
+inputEmail.addEventListener('onchange', validateEmail);
+inputPassword.addEventListener('onchange', validatePassword);
